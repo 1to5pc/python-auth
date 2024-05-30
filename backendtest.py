a6a7314fffd0e9eb0e298b93b3b8ff10116af8b2
ravinu <github@swore3556.anonaddy.me>
2024-05-30 09:34:57 +0530

    Update to test v2.0

--- backendtest.py
+++ backendtest.py
@@ -1,11 +1,12 @@
 import usrcheck
 import hashlib
 def test_auth():
-    usrcheck.save_users([['testusr',hashlib.sha256('testPSWD'.encode()).hexdigest()]],True)
-    assert usrcheck.load_users() == [['testusr',hashlib.sha256('testPSWD'.encode()).hexdigest()]], "Userlist loading failed"
-    assert usrcheck.usr_check('testusr', 'testPSWD',[['testusr',hashlib.sha256('testPSWD'.encode()).hexdigest()]]) == (True,True), "User check failed"
+    usr_list=[['testusr',hashlib.sha256(str('testPSWD'+'1QgF35ws').encode()).hexdigest(),'1QgF35ws']]
+    usrcheck.save_users(usr_list,True)
+    assert usrcheck.load_users() == usr_list, "Userlist loading failed"
+    assert usrcheck.usr_check('testusr', 'testPSWD',usr_list) == (True,True), "User check failed"
     #Wrong username
-    assert usrcheck.usr_check('wrongusr', 'testPSWD',[['testusr',hashlib.sha256('testPSWD'.encode()).hexdigest()]]) == (False,False), "User check failed"
+    assert usrcheck.usr_check('wrongusr', 'testPSWD',usr_list) == (False,False), "User check failed"
     #Wrong password
-    assert usrcheck.usr_check('testusr', 'wrongPSWD',[['testusr',hashlib.sha256('testPSWD'.encode()).hexdigest()]]) == (True,False), "User check failed"
+    assert usrcheck.usr_check('testusr', 'wrongPSWD',usr_list) == (True,False), "User check failed"
 test_auth()
