345cfd074642d0ce19347d37688157ff0f2d184f
Colby-Coffman <colbycoffman20@gmail.com>
2024-05-30 13:27:10 -0500

    Changed the backend test to test all possible hashing algorithms that the python interpreter is aware of on a given users machine

--- backendtest.py
+++ backendtest.py
@@ -1,12 +1,16 @@
 import usrcheck
-import hashlib
+import hash
 def test_auth():
-    usr_list=[['testusr',hashlib.sha256(str('testPSWD'+'1QgF35ws').encode()).hexdigest(),'1QgF35ws']]
-    usrcheck.save_users(usr_list,True)
-    assert usrcheck.load_users() == usr_list, "Userlist loading failed"
-    assert usrcheck.usr_check('testusr', 'testPSWD',usr_list) == (True,True), "User check failed"
-    #Wrong username
-    assert usrcheck.usr_check('wrongusr', 'testPSWD',usr_list) == (False,False), "User check failed"
-    #Wrong password
-    assert usrcheck.usr_check('testusr', 'wrongPSWD',usr_list) == (True,False), "User check failed"
+    # Updated test to support new implementation
+    for hash_method in hash.SUPPORTED_HASHES:
+        hasher = hash.Hasher(hash_method, ('testPSWD' + '1QgF35ws').encode())
+        usr_list = [['testusr', hasher.hexdigest(), '1QgF35ws']]
+        hasher.clear_hasher()
+        usrcheck.save_users(usr_list, True)
+        assert usrcheck.load_users() == usr_list, "UserList loading failed"
+        assert usrcheck.usr_check('testusr', 'testPSWD', usr_list, test_hasher=hasher) == (True,True), "User check failed"
+        #Wrong username
+        assert usrcheck.usr_check('wrongusr', 'testPSWD', usr_list, test_hasher=hasher) == (False,False), "User check failed"
+        #Wrong password
+        assert usrcheck.usr_check('testusr', 'wrongPSWD', usr_list, test_hasher=hasher) == (True,False), "User check failed"
 test_auth()
