89b0c534e5ace9fefe72e4b4e56a193d8a061a01
SenhasD <144352083+SenhasD@users.noreply.github.com>
2024-06-02 13:30:29 +0530

    Update usrcheck.py
    A lot of refractoring
    Functions with clearer names (config_read, salter, init_users, etc.).
    Improved error handling for reading configuration values and setting defaults.
    Consistent use of string methods and formatting.
    Added type annotations for better readability and type checking.
    Simplified the main menu logic and user prompts.

--- usrcheck.py
+++ usrcheck.py
@@ -1,221 +1,175 @@
+import configparser
+import string
+import secrets
+import os
+import sys
+from typing import List, Tuple
 import hash
 
-# TODO: Encapsulate this later
+# Global variable for the hasher
 hasher = None
 
-def configRead():
-    import configparser
+def config_read() -> Tuple[bool, str, int]:
+    """Reads the configuration from the config.ini file."""
     config = configparser.ConfigParser()
     config.read('config.ini')
     try:
-        quiet = config['Config']['quiet']
-        quiet = quiet.lower() in ("true")
+        quiet = config['Config']['quiet'].lower() in ("true")
     except KeyError:
         quiet = False
+    
     try:
         alg = config['Config']['alg'].lower()
-        # alg = str(quiet).lower() Not really sure the purpose of this line
-        # Not the most elegant but trying to hit the KeyError on unsupported hash algs
-        if not (alg in hash.SUPPORTED_HASHES):
+        if alg not in hash.SUPPORTED_HASHES:
             raise KeyError
     except KeyError:
-        print("ERROR: Algorithm not defined. Reverting to SHA256")
+        print("ERROR: Algorithm not defined or unsupported. Reverting to SHA256")
         alg = 'sha256'
+    
     global hasher
     hasher = hash.Hasher(alg)
+    
     try:
-        saltSize = config['Salt']['saltSize']
-        saltSize = int(saltSize)
-    except KeyError:
-        saltSize = 8
-        print("ERROR: saltSize not defined or not integer. Reverting to 8")
-    return quiet,alg,saltSize
+        salt_size = int(config['Salt']['saltSize'])
+    except (KeyError, ValueError):
+        salt_size = 8
+        print("ERROR: saltSize not defined or not an integer. Reverting to 8")
+    
+    return quiet, alg, salt_size
 
-def salter(pswd,saltsize):
-    import string
-    import secrets
+def salter(pswd: str, salt_size: int) -> Tuple[str, str]:
+    """Generates a salt and returns the salted password and salt."""
     alphabet = string.ascii_letters + string.digits
-    salt = ''.join(secrets.choice(alphabet) for i in range(saltsize))
-    return pswd+salt,salt
+    salt = ''.join(secrets.choice(alphabet) for _ in range(salt_size))
+    return pswd + salt, salt
 
-def init_usrs(nUsr, saltSize):
-    usrlist=[]
-    for index in range(nUsr):
-        usrname=''
-        pswd=''
-        while usrname=='':
-            usrname = input("Enter username: ")
-        while pswd=='':
-            pswd = input("Enter password: ")
-        pswd,salt = salter(pswd,saltSize)
-        hasher.update(pswd.encode())
-        pswd = hasher.hexdigest()
-        # We have to clear the hasher every time with the new implementation, later we could define this behaviour automatically
+def init_users(n_usr: int, salt_size: int) -> List[List[str]]:
+    """Initializes user accounts with salted and hashed passwords."""
+    user_list = []
+    for index in range(n_usr):
+        username = input("Enter username: ").strip()
+        while not username:
+            username = input("Enter username: ").strip()
+        
+        password = input("Enter password: ").strip()
+        while not password:
+            password = input("Enter password: ").strip()
+        
+        salted_password, salt = salter(password, salt_size)
+        hasher.update(salted_password.encode())
+        hashed_password = hasher.hexdigest()
         hasher.clear_hasher()
-        usrlist.append([])
-        usrlist[index].append(usrname)
-        usrlist[index].append(pswd)
-        usrlist[index].append(salt)
-    return usrlist
+        
+        user_list.append([username, hashed_password, salt])
+    
+    return user_list
 
-def load_users():
-    usrlist=[]
-    saveFile = open("usrlist", "r")
-    temp = saveFile.read().split(",")
-    for x in range(len(temp)//3):
-        usrlist.append([])
-        usrlist[x].append(temp[(3*x)])
-        usrlist[x].append(temp[(3*x)+1])
-        usrlist[x].append(temp[(3*x)+2])
-    saveFile.close()
-    return usrlist
+def load_users() -> List[List[str]]:
+    """Loads user data from the usrlist file."""
+    user_list = []
+    with open("usrlist", "r") as file:
+        data = file.read().split(",")
+        for i in range(0, len(data), 3):
+            user_list.append(data[i:i+3])
+    return user_list
 
-def save_users(usrlist,overWrite):
-    if overWrite==True:
-        saveFile = open("usrlist", "w")
-    else:
-        saveFile = open("usrlist", "r")
-        if saveFile.read()!='':
-            saveFile = open("usrlist", "a")
-            saveFile.write(',')
-        else:
-            saveFile = open("usrlist", "a")
-    if len(usrlist)>0:
-        for x in range(len(usrlist)):
-            if (x+1)<len(usrlist):
-                saveFile.write(str(usrlist[x][0])+","+str(usrlist[x][1])+","+str(usrlist[x][2])+',')
-            else:
-                saveFile.write(str(usrlist[x][0])+","+str(usrlist[x][1])+","+str(usrlist[x][2]))
-    else:
-        saveFile.write('')
-    saveFile.close()
-   
-def usr_check(usrn,pswd,usrlist, test_hasher: hash.Hasher=None):
+def save_users(user_list: List[List[str]], overwrite: bool) -> None:
+    """Saves user data to the usrlist file."""
+    mode = 'w' if overwrite else 'a'
+    with open("usrlist", mode) as file:
+        if not overwrite and os.path.getsize("usrlist") > 0:
+            file.write(',')
+        for i, user in enumerate(user_list):
+            file.write(",".join(user))
+            if i < len(user_list) - 1:
+                file.write(',')
+
+def user_check(username: str, password: str, user_list: List[List[str]], test_hasher: hash.Hasher = None) -> Tuple[bool, bool]:
+    """Checks if the username and password match any entry in the user list."""
     if test_hasher:
         hasher = test_hasher
-    # TODO:
-    # Supporting different hashes means we have to figure out what hash was used for the usrlist
-    # This basic solution simply reads what hash was specified in the config file.
-    # The config file will need additional information, and this logic will have to be changed, if variable
-    # output for shake is allowed.
     else:
-        import configparser
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        try:
-            alg = config['Config']['alg'].lower()
-            if not (alg in hash.SUPPORTED_HASHES):
-                raise KeyError
-        except KeyError:
-            print("No algorithm specified to read usrlist or unsuporrted algorithm\nDefaulting to sha256")
-            alg = 'sha256'
-        hasher = hash.Hasher(alg)
-    index=0
-    Ufound=False
-    Pfound=False
-    while Ufound==False and index<len(usrlist):
-        if usrlist[index][0]==usrn:
-            Ufound=True
-            hasher.update(str(pswd+str(usrlist[index][2])).encode())
-            pswd = hasher.hexdigest()
+        config_read()
+    
+    for user in user_list:
+        if user[0] == username:
+            hasher.update((password + user[2]).encode())
+            hashed_password = hasher.hexdigest()
             hasher.clear_hasher()
-            if usrlist[index][1]==pswd:
-                Pfound=True
-            break
-        index=index+1
-    return Ufound,Pfound
+            return True, hashed_password == user[1]
+    return False, False
 
-def login_status(Ufound,Pfound,quiet):
-    if quiet==False:
-        if Ufound==True and Pfound==True:
+def login_status(user_found: bool, password_correct: bool, quiet: bool) -> bool:
+    """Prints the login status based on the quiet mode and the authentication results."""
+    if not quiet:
+        if user_found and password_correct:
             print("Access authorized")
-            return True
-        elif Ufound==True:
+        elif user_found:
             print("Incorrect password. Access denied")
-            return False
         else:
             print("User does not exist. Access denied")
-            return False
-    else:
-        if Ufound==True and Pfound==True:
-            return True
-        else:
-            return False
-            
-def login_init(usrname,pswd,overWrite,quiet,saltSize):
-    import os
-    import sys
-    loginSt=False
-    if os.path.isfile("usrlist") == False and overWrite==False:
-        print("Userfile does not exist! Execute with overWrite set to 'True'")
+    return user_found and password_correct
+
+def login_init(username: str, password: str, overwrite: bool, quiet: bool, salt_size: int) -> bool:
+    """Initializes the login process based on the given parameters."""
+    if not os.path.isfile("usrlist") and not overwrite:
+        print("User file does not exist! Execute with overwrite set to 'True'")
         sys.exit()
-    elif overWrite==True:
-        # Bug fix for when no usrlist exists but the user asks not to overwrite user list
-        newFile = False
-        if not(os.path.isfile("usrlist")):
-            newFile = True
-        elif os.path.isfile("usrlist") and (input("Do you want to overwrite the existing user list [y/N]? ").upper() == 'Y'):
-            newFile = True
+    
+    if overwrite:
+        new_file = not os.path.isfile("usrlist")
+        if os.path.isfile("usrlist") and input("Do you want to overwrite the existing user list [y/N]? ").upper() == 'Y':
+            new_file = True
+        
         while True:
-            # TODO: 
-            # Seems to me redundant to ask if they want to clear the database when "overwriting" the existing database does
-            # the same thing.
-            nUsr=input("Enter the number of users to initialize ('0' Clears the database): ")
+            n_usr = input("Enter the number of users to initialize ('0' Clears the database): ")
             try:
-                nUsr=int(nUsr)
-                if int(nUsr)<0:
+                n_usr = int(n_usr)
+                if n_usr < 0:
                     print('The value must not be negative.')
                 else:
                     break
             except ValueError:
                 pass
-        if nUsr>0:
-            usrlist=init_usrs(nUsr,saltSize)
-            save_users(usrlist,newFile)
-        elif nUsr==0 and overWrite==True:
-            save_users([],True)
+        
+        if n_usr > 0:
+            user_list = init_users(n_usr, salt_size)
+            save_users(user_list, new_file)
+        elif n_usr == 0:
+            save_users([], True)
+        return False
     else:
-        usrlist = load_users()
-        Ufound,Pfound=usr_check(usrname,pswd,usrlist)
-        loginSt=login_status(Ufound,Pfound,quiet)
-    return loginSt
+        user_list = load_users()
+        user_found, password_correct = user_check(username, password, user_list)
+        return login_status(user_found, password_correct, quiet)
 
+def auth_test(quiet: bool) -> None:
+    """Conducts an authentication test."""
+    config_read()
+    print("[Authentication Test]".center(os.get_terminal_size().columns))
+    username = input("Input username: ").strip()
+    password = input("Input password: ").strip()
+    login_success = login_init(username, password, False, quiet, 0)
+    print(f"Login success status: {login_success}")
 
-def Auth_test(quiet):
-    import os
-    configRead()
-    try:
-        print("[Authentication Test]".center(os.get_terminal_size().columns))
-    except OSError:
-        print("[Authentication Test]")
-    usr=input("Input username: ")
-    pswd=input("Input password: ")
-#    if input("Enable quiet mode? [y/N]").upper() == "Y":
-#        quiet=True
-#    else:
-#        quiet=False
-    loginSuccess=login_init(usr,pswd,False,quiet,0)
-    if quiet!=True:
-        print("Login success status:", loginSuccess)
-    else:
-        print(loginSuccess)
-
-def main_menu():
-    quiet,alg,saltSize=configRead()
+def main_menu() -> None:
+    """Displays the main menu and handles user choices."""
+    quiet, alg, salt_size = config_read()
     print("\n--- Main Menu ---")
     print("0. Initialize user accounts")
     print("1. Test Authentication")
     print("2. Exit")
-    choice = input("Enter your choice: ")
+    choice = input("Enter your choice: ").strip()
+    
     if choice == '0':
-        login_init('','',True,quiet,saltSize)
+        login_init('', '', True, quiet, salt_size)
     elif choice == '1':
-        Auth_test(quiet)
+        auth_test(quiet)
     elif choice == '2':
         print("Exiting program.")
-        exit()
+        sys.exit()
     else:
-        print("Invalid choice. Please enter 0, 1 or 2.")
+        print("Invalid choice. Please enter 0, 1, or 2.")
         main_menu()
 
 if __name__ == "__main__":
