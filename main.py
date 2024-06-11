c0ed5cc6f82a816ee81287c72b01ed63549d701c
ravinu <github@swore3556.anonaddy.me>
2024-06-11 07:20:03 +0530

    Add support for ASCII Art toggle

--- main.py
+++ main.py
@@ -2,27 +2,29 @@
 import art
 import usrcheck
 import random
-# initialise ASCII art
-# Define the ASCII art for the text
-text_art = r"""
-  ____        _   _                 
- |  _ \ _   _| |_| |__   ___  _ __  
- | |_) | | | | __| '_ \ / _ \| '_ \ 
- |  __/| |_| | |_| | | | (_) | | | |
- |_|    \__, |\__|_| |_|\___/|_| |_|
-        |___/                       
-"""
+quiet,alg,saltSize,artConf=usrcheck.configRead()
+if artConf==True:
+  # initialise ASCII art
+  # Define the ASCII art for the text
+  text_art = r"""
+    ____        _   _                 
+   |  _ \ _   _| |_| |__   ___  _ __  
+   | |_) | | | | __| '_ \ / _ \| '_ \ 
+   |  __/| |_| | |_| | | | (_) | | | |
+   |_|    \__, |\__|_| |_|\___/|_| |_|
+          |___/                       
+  """
 
-# Define the ASCII art for the cat
-cat_art = r"""
- /\_/\  
-( o.o ) 
- > ^ <
-"""
-# initialise other required python files
+  # Define the ASCII art for the cat
+  cat_art = r"""
+   /\_/\  
+  ( o.o ) 
+   > ^ <
+  """
+  # initialise other required python files
 
-combined_art = art.insert_cat_randomly(text_art, cat_art)
-print(combined_art)
+  combined_art = art.insert_cat_randomly(text_art, cat_art)
+  print(combined_art)
 usrcheck.main_menu()
 
 
